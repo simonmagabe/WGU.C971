8ddaee3bb803b91dcd1ddb8eed60b3869c12463d
simblitz@gmail.com <simblitz@gmail.com>
2022-02-11 15:12:28 -0500

    Add Assessment and Course model classes.

---
+++ WGU.C971/WGU.C971/Models/Assessment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WGU.C971.Models
+{
+    [Table("Assessment")]
+    public class Assessment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Type { get; set; }
+    }
+}
